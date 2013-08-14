877b4caa9d84514058bcb451d9e794e60c8e83ea
Vincent Zhu <zhu1230@gmail.com>
2013-08-15 02:02:06 +0800

    clean up

--- lib/orientea/generators/command/command_generator.rb
+++
@@ -1,7 +0,0 @@
-class CommandGenerator < Rails::Generators::Base
-  source_root File.expand_path('./templates', __FILE__)  
-  desc "rails g orientea:command"
-  def create_migration
-    migration_template 'create_command.rb', 'db/migrate/create_command_table.rb'
-  end
-end
