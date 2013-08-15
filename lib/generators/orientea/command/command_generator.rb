a14f95d6befd15f68826c590dd94fb5c2cfb8f6a
Vincent Zhu <zhu1230@gmail.com>
2013-08-15 12:41:46 +0800

    finish migration generator

--- lib/generators/orientea/command/command_generator.rb
+++ lib/generators/orientea/command/command_generator.rb
@@ -1,10 +1,15 @@
  module Orientea
   class CommandGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
+
     source_root File.expand_path('../templates', __FILE__)  
     # desc "rails g orientea:command"
     def create_migration
       migration_template 'create_command.rb', 'db/migrate/create_command_table.rb'
+    end
+
+    def self.next_migration_number(dir)
+      ActiveRecord::Migration.next_migration_number(dir.to_i)
     end
   end
 end
--- lib/generators/orientea/command/templates/create_command.rb
+++ lib/generators/orientea/command/templates/create_command.rb
@@ -1,7 +1,14 @@
-class CreateOrienteaCommand < ActiveRecord::Migration
-  create_table :orientea_commands do |t|
-   t.hstore :data
-   t.boolean :done
-   t.string :type
+class CreateCommandTable < ActiveRecord::Migration
+  def up
+    execute "CREATE EXTENSION IF NOT EXISTS hstore"
+    create_table :orientea_commands do |t|
+      t.hstore :data
+      t.boolean :done
+      t.string :type
+    end
+  end
+
+  def down
+    drop_table :orientea_commands
   end
 end
